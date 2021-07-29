cbdfa6b30611c0f88a5b0f98b95bbcbc6405364d
User53 <56277379+User53-official@users.noreply.github.com>
2021-07-28 20:50:01 -0400

    fix: Updated according to issue #2

--- CompileElm.pl
+++ CompileElm.pl
@@ -2,5 +2,5 @@
 opendir(Pages, "src/Pages");
 my @files = map { "src/Pages/$_" } grep !/^\./, readdir(Pages);
 $, = " ";
-my $command = "elm make @files @ARGV --output=../public/app.js";
+my $command = "elm make @files @ARGV --output=public/app.js";
 system($command);
